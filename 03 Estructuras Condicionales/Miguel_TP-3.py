56f61462128e075a6221b56520b84c9f3d3a350d
eDaNi-M <danimiguel9626@gmail.com>
2025-05-08 20:50:05 -0300

    comienzo de tp 3

---
+++ 03 Estructuras Condicionales/Miguel_TP-3.py
@@ -0,0 +1,61 @@
+# Ejercicio 1
+edad1 = int(input("ingrese su edad"))
+
+if edad1 > 18 :
+    print ("Eres mayor de edad")
+else:
+    print("intentalo de nuevo en unos años")
+
+
+# Ejercicio 2
+nota = float(input("Ingrese su nota"))
+
+if nota >= 6:
+    print ("Aprobado")
+else:
+    print ("Desaprobaste mi rey, mejor suerte la proxima")
+
+
+# Ejercicio 3
+numeroPar = int(input("Ingrese un numero par"))
+
+if numeroPar % 2 == 0:
+    print("es un numero par")
+else:
+    print("Por favor, Ingrese un numero par")
+
+
+# Ejercicio 4
+edad4 = int(input("Ingrese su edad"))
+
+if edad4 < 12:
+    print("Eres un niño")
+elif edad4 >= 12 and edad4 < 18:
+    print ("Eres un adolescente")
+elif edad4 >= 18 and edad4 <30:
+    print("Eres un adulto joven")
+else:
+    print("Eres un adulto")
+
+
+# Ejercicio 5
+
+
+
+# Ejercicio 6
+
+
+
+# Ejercicio 7
+
+
+
+# Ejercicio 8
+
+
+
+# Ejercicio 9
+
+
+
+# Ejercicio 10
